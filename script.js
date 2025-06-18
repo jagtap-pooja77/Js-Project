aa6a04323cd29c7c2f6fd3342512f233a7fbdb4f
pooja jagtap <poojajagtap0613@gmail.com>
2025-06-18 09:28:13 +0530

    update the guessing number file

--- script.js
+++ script.js
@@ -5,21 +5,30 @@ function submitGuess() {
   const guess = Number(document.getElementById('text-1').value);
   const message = document.getElementById('message');
   const attemptsDisplay = document.getElementById('attempts');
-  attempts++;
+ 
 
-  if (guess < 1 || guess > 100) {
+
+   if (guess < 1 || guess > 100) {
     message.textContent = "Please enter a number between 1 and 100.";
     message.className = "wrong";
   } else if (guess < randomNumber) {
-    message.textContent = "Too low! Try again.";
+    message.textContent = '📉 Too low';
+    message.style.color = 'red';
     message.className = "wrong";
+     attempts++;
   } else if (guess > randomNumber) {
-    message.textContent = "Too high! Try again.";
+    message.textContent = '📈 Too high';
+     message.style.color = 'green';
     message.className = "wrong";
+     attempts++;
   } else {
     message.innerHTML = `🎉 <span class="correct">Correct! The number was ${randomNumber}.<br>You guessed it in ${attempts} attempts.</span>`;
     message.className = "correct";
+    message.style.color = 'green';
   }
 
   attemptsDisplay.textContent = `Attempts: ${attempts}`;
 }
+
+  
+ 
